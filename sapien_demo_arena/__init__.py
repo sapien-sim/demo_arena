2f4b39016e0a454a20e5b16ceec45585b33403e8
Fanbo Xiang <fxiang@eng.ucsd.edu>
2023-10-23 22:38:04 -0700

    add light & rename

--- sapien_demo_arena/__init__.py
+++ sapien_demo_arena/__init__.py
@@ -2,12 +2,12 @@ import sapien
 from pathlib import Path
 
 
-class DefaultArena(sapien.Widget):
+class DemoArena(sapien.Widget):
     def load(self, scene: sapien.Scene):
         scene.set_environment_map(str(Path(__file__).parent / "env.ktx"))
         self.ground = scene.add_ground(0)
-        # self.light = scene.add_directional_light([1, 1, -1], [1, 1, 1], True)
+        self.light = scene.add_directional_light([1, 1, -1], [1, 1, 1], True)
 
     def unload(self, scene: sapien.Scene):
         scene.remove_actor(self.ground)
-        # scene.remove_light(self.light)
+        scene.remove_light(self.light)
